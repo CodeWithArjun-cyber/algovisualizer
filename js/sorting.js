c100a361143c32f38ba5bd19914bffd0fe95abf4
Arjun <arjunprajapati3786@gmail.com>
2021-11-24 12:49:47 +0530

    adding sorting file

--- js/sorting.js
+++ js/sorting.js
@@ -1,16 +1,15 @@
-
-// swap function for sorting algorithms which takes input of 2 dom elements with .style.height feature
+// swap function util for sorting algorithms takes input of 2 DOM elements with .style.height feature
 function swap(el1, el2) {
-    console.log('IN swap()');
-
+    console.log('In swap()');
+    
     let temp = el1.style.height;
     el1.style.height = el2.style.height;
     el2.style.height = temp;
+    
 }
 
-
-// Disables sortin buttion used in conjuction with enable, so that we can disable during sorting and enable after it.
-function disableSortingBtn() {
+// Disables sorting buttons used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableSortingBtn(){
     document.querySelector(".bubbleSort").disabled = true;
     document.querySelector(".insertionSort").disabled = true;
     document.querySelector(".mergeSort").disabled = true;
@@ -18,8 +17,8 @@ function disableSortingBtn() {
     document.querySelector(".selectionSort").disabled = true;
 }
 
-//Enables sorting buttons used in cojunction with disable.
-function enableSortingBtn() {
+// Enables sorting buttons used in conjunction with disable
+function enableSortingBtn(){
     document.querySelector(".bubbleSort").disabled = false;
     document.querySelector(".insertionSort").disabled = false;
     document.querySelector(".mergeSort").disabled = false;
@@ -27,105 +26,97 @@ function enableSortingBtn() {
     document.querySelector(".selectionSort").disabled = false;
 }
 
-// Disable size slider used in conjuction with enable , so that we can disable during sorting and enable button after it.
-function disableSizeSlider() {
+// Disables size slider used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableSizeSlider(){
     document.querySelector("#arr_sz").disabled = true;
 }
 
-
-//Enable size slider used in conjuction with disable
-function enableSizeSlider() {
+// Enables size slider used in conjunction with disable
+function enableSizeSlider(){
     document.querySelector("#arr_sz").disabled = false;
 }
 
-
-//Disable newArray Button 
-function disableNewArrayBtn() {
+// Disables newArray buttons used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableNewArrayBtn(){
     document.querySelector(".newArray").disabled = true;
 }
 
-// Enable newArray Button 
-function enableNewArrayBtn() {
+// Enables newArray buttons used in conjunction with disable
+function enableNewArrayBtn(){
     document.querySelector(".newArray").disabled = false;
 }
 
-
-// Used in async function so that we can so animation of sorting, takes input time in ms (1000 = 1s)
-function waitforme(millisec) {
-    return new Promise(resolve => {
-        setTimeout(() => { resolve('') }, millisec);
-    })
+// Used in async function so that we can so animations of sorting, takes input time in ms (1000 = 1s)
+function waitforme(milisec) { 
+    return new Promise(resolve => { 
+        setTimeout(() => { resolve('') }, milisec); 
+    }) 
 }
 
-
-//Selecting size slider from DOM
+// Selecting size slider from DOM
 let arraySize = document.querySelector('#arr_sz');
 
-//Event Listener to update the bars on the UI 
-arraySize.addEventListener('input', function () {
-    console.log(arraySize.value, typeof (arraySize.value));
+// Event listener to update the bars on the UI
+arraySize.addEventListener('input', function(){
+    console.log(arraySize.value, typeof(arraySize.value));
     createNewArray(parseInt(arraySize.value));
 });
 
-
-//Default input for waitforme funtion
+// Default input for waitforme function (260ms)
 let delay = 260;
 
-//selection speed slider from DOM
+// Selecting speed slider from DOM
 let delayElement = document.querySelector('#speed_input');
 
-//Event listener to update delay time
-delayElement.addEventListener('input', function () {
-    console.log(delayElement.value, typeof (delayElement.value));
+// Event listener to update delay time 
+delayElement.addEventListener('input', function(){
+    console.log(delayElement.value, typeof(delayElement.value));
     delay = 320 - parseInt(delayElement.value);
 });
 
-
-//Generating array to store randomly generated numbers
+// Creating array to store randomly generated numbers
 let array = [];
 
-//call to display bars input size of array
+// Call to display bars right when you visit the site
 createNewArray();
 
-//To Create new array input size of array
+// To create new array input size of array
 function createNewArray(noOfBars = 60) {
-    //calling helper function to delete old bars frm dom
+    // calling helper function to delete old bars from dom
     deleteChild();
 
-    //creating an array of random numbers
+    // creating an array of random numbers 
     array = [];
     for (let i = 0; i < noOfBars; i++) {
         array.push(Math.floor(Math.random() * 250) + 1);
     }
     console.log(array);
 
-    //select the div #bars element
+    // select the div #bars element
     const bars = document.querySelector("#bars");
 
-    // Create multiple element div using loop andding class 'bar col '
+    // create multiple element div using loop and adding class 'bar col'
     for (let i = 0; i < noOfBars; i++) {
         const bar = document.createElement("div");
         bar.style.height = `${array[i]*2}px`;
         bar.classList.add('bar');
         bar.classList.add('flex-item');
-        bar.classList.add(`barsNo${i}`);
-        bar.appendChild(bar);
+        bar.classList.add(`barNo${i}`);
+        bars.appendChild(bar);
     }
 }
 
-
-//Helper funtion to delete all the previous bars so that new can be added
-function deleteChild(){
+// Helper function to delete all the previous bars so that new can be added
+function deleteChild() {
     const bar = document.querySelector("#bars");
     bar.innerHTML = '';
 }
 
-
-//Selecting newarray button from DOM and adding eventlistner
+// Selecting newarray button from DOM and adding eventlistener
 const newArray = document.querySelector(".newArray");
-newArray.addEventListener("click",function(){
-    console.log("From newArray "+ arraySize.value);
-    console.log("From newArray "+delay);
+newArray.addEventListener("click", function(){
+    console.log("From newArray " + arraySize.value);
+    console.log("From newArray " + delay);
     enableSortingBtn();
     enableSizeSlider();
     createNewArray(arraySize.value);
